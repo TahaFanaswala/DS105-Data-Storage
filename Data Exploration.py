94f67343281fdaa00d9320a47a15e2d2bc62d5bc
Taha Fanaswala <fanaswal@usc.edu>
2022-05-24 15:51:05 +0100

    Tracked null percent nationally over time for multi year attributes

--- Data Exploration.py
+++ Data Exploration.py
@@ -1,7 +1,9 @@
 import os
 
+import matplotlib
 import pandas as pd
 import numpy as np
+import math
 
 import warnings
 
@@ -9,7 +11,8 @@ warnings.simplefilter("ignore")
 
 import seaborn as sns
 import matplotlib.pyplot as plt
-
+import matplotlib.patches as mpatches
+import matplotlib
 from collections import Counter
 
 states = ['AK', 'AL', 'AR', 'AZ', 'CA', 'CO', 'CT', 'DC', 'DE', 'FL', 'GA',
@@ -305,3 +308,93 @@ def percentAdjustNullCounts():
         xlw.close()
 
 
+def generateStackedBarChartOneYearAttributesNullCount():
+    instCountOverTime = pd.read_excel("Visualization Datasets/instiution count data.xlsx",
+                                      sheet_name="Sheet1").set_index("Unnamed: 0")
+    oneYearAttrNullCounts = pd.read_excel("Visualization Datasets/One Year Attributes Null Counts.xlsx",
+                                          sheet_name="Sheet1").set_index("Unnamed: 0")
+
+    oneYearOnly = ["CDR3",
+                   "C150_4_WHITE", "C150_4_BLACK", "C150_4_HISP", "C150_4_ASIAN", "C150_4_NHPI",
+                   "C150_4_NRA", "C150_4_UNKN"]
+    YEAR = 2013
+
+    transformedData = pd.DataFrame()
+    totalInst = sum(instCountOverTime[YEAR])
+    for attr in oneYearOnly:
+        sumNulls = sum(oneYearAttrNullCounts[attr])
+
+        transformedData.at[attr, "Null"] = sumNulls / totalInst
+        transformedData.at[attr, "Not Null"] = 1
+
+    transformedData.reset_index(inplace=True)
+    transformedData.rename(columns={"index": "Attribute"}, inplace=True)
+
+    matplotlib.rc("axes", labelsize=20)
+    matplotlib.rc("font", size=15)
+
+    bar1 = sns.barplot(x="Attribute", y="Not Null", data=transformedData, color="blue")
+    bar2 = sns.barplot(x="Attribute", y="Null", data=transformedData, color="red")
+
+    topBar = mpatches.Patch(color="blue", label="Not Null")
+    bottomBar = mpatches.Patch(color="red", label="Null")
+    plt.legend(handles=[topBar, bottomBar])
+    plt.ylabel("Percent")
+
+    plt.show()
+
+
+def timeSeriesNullPercentageForMultiYearAttributes():
+    instCountOverTime = pd.read_excel("Visualization Datasets/instiution count data.xlsx",
+                                      sheet_name="Sheet1").set_index("Unnamed: 0")
+
+    multiYear = ["TUITIONFEE_IN", "TUITIONFEE_OUT", "UGDS_WHITE", "UGDS_BLACK", "UGDS_HISP", "UGDS_ASIAN"]
+
+    transformedData = pd.DataFrame()
+    index = 0
+
+    saveVal = pd.DataFrame()
+
+    for attr in multiYear:
+        path = f"Visualization Datasets\\{attr} Null Counts Over Time.xlsx"
+        data = pd.read_excel(path, sheet_name="Sheet1").set_index("Unnamed: 0")
+        for year in instCountOverTime.columns:
+            sumInst = sum(instCountOverTime[year])
+            totalNulls = sum(data[year])
+
+            transformedData.at[index, "Attribute"] = attr
+            transformedData.at[index, "Year"] = year
+            transformedData.at[index, "Null Percent"] = totalNulls / sumInst
+
+            index += 1
+
+            saveVal.at[attr, year] = totalNulls / sumInst
+
+
+        saveVal.to_csv(path_or_buf="sanity check.csv")
+
+    transformedData["Year"] = transformedData["Year"].astype(int)
+
+    matplotlib.rc("axes", labelsize=20)
+    matplotlib.rc("font", size=15)
+
+    sns.lineplot(data=transformedData, x="Year", y="Null Percent", hue="Attribute")
+
+    plt.xticks(computeTicks(transformedData["Year"], step=1))
+
+    plt.show()
+
+
+def computeTicks(x, step=5):
+    """
+    Computes domain with given step encompassing series x
+    @ params
+    x    - Required - A list-like object of integers or floats
+    step - Optional - Tick frequency
+    """
+    xMax, xMin = math.ceil(max(x)), math.floor(min(x))
+    dMax, dMin = xMax + abs((xMax % step) - step) + (step if (xMax % step != 0) else 0), xMin - abs((xMin % step))
+    return range(dMin, dMax, step)
+
+
+timeSeriesNullPercentageForMultiYearAttributes()
